8d9e3e296a55e8b3a5ec59d94442566dabe0435b
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2024-12-06 19:34:43 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Vivid AI",
+  description: "Transform your photos with Vivid AI, the ultimate AI-powered image enhancer. Enhance resolution, clarity, and vibrancy with cutting-edge technology designed for photographers, designers, and anyone seeking picture-perfect results. Experience stunning photo quality with ease—no technical skills required. Try Vivid AI today and elevate your visuals to the next level!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
